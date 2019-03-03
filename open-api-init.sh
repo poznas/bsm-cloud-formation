b194e256f9a367dc518e54187fa1d6ae9eef69f4
poznas <misterdannypl@gmail.com>
2019-03-04 00:18:13 +0100

    start open-api as systemd service, attach logback-spring.xml

--- open-api-init.sh
+++ open-api-init.sh
@@ -6,11 +6,18 @@ mv jdk-11.0.1 /usr/local/
 echo 'export JAVA_HOME=/usr/local/jdk-11.0.1;export PATH=$PATH:$JAVA_HOME/bin' | sudo tee /etc/profile.d/jdk11.sh
 source /etc/profile.d/jdk11.sh
 
-chown ec2-user:ec2-user /home/ec2-user/open-api.jar
-chmod 500 /home/ec2-user/open-api.jar
-
-ln -sf /home/ec2-user/open-api.jar /etc/init.d/open-api
 ln -sf /usr/local/jdk-11.0.1/bin/java /sbin/java
 
-service open-api start
+aws s3 cp s3://repo.bsm.pub/cloud-formation-init/open-api.env /etc/default/open-api.env
+aws s3 cp s3://repo.bsm.pub/cloud-formation-init/open-api.service /etc/systemd/system/open-api.service
+aws s3 cp s3://repo.bsm.pub/cloud-formation-init/open-api.conf /etc/rsyslog.d/open-api.conf
+aws s3 cp s3://repo.bsm.pub/cloud-formation-init/logback-spring.xml /opt/open-api/logback-spring.xml
 
+chmod -R 500 /opt/open-api
+mkdir /opt/open-api/logs
+
+chown -R ec2-user:ec2-user /opt/open-api
+
+systemctl daemon-reload
+systemctl enable open-api
+systemctl start open-api
