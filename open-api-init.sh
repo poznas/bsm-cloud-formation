539b3f31af09fe788b74c5f60d7da1c5eebf62b4
poznas <misterdannypl@gmail.com>
2019-03-03 19:07:56 +0100

    start open-api as init.d service

--- open-api-init.sh
+++ open-api-init.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
 curl -O https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
 tar zxvf openjdk-11.0.1_linux-x64_bin.tar.gz
-sudo mv jdk-11.0.1 /usr/local/
+mv jdk-11.0.1 /usr/local/
 echo 'export JAVA_HOME=/usr/local/jdk-11.0.1;export PATH=$PATH:$JAVA_HOME/bin' | sudo tee /etc/profile.d/jdk11.sh
 source /etc/profile.d/jdk11.sh
-aws s3 cp s3://repo.bsm.pub/release/com/bsm/application/0.0.22/application-0.0.22.jar /home/ec2-user/application-0.0.22.jar
-nohup java -jar application-0.0.22.jar
+
+chown ec2-user:ec2-user /home/ec2-user/open-api.jar
+chmod 500 /home/ec2-user/open-api.jar
+
+ln -sf /home/ec2-user/open-api.jar /etc/init.d/open-api
+ln -sf /usr/local/jdk-11.0.1/bin/java /sbin/java
+
+service open-api start
+
