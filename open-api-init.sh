f08fceba75a18758244293f3a2e88468a79fb0ff
poznas <misterdannypl@gmail.com>
2019-03-18 22:52:56 +0100

    run local vault server as systemd service

--- open-api-init.sh
+++ open-api-init.sh
@@ -19,13 +19,32 @@ mkdir /opt/open-api/logs
 
 chown -R ec2-user:ec2-user /opt/open-api
 
-wget https://releases.hashicorp.com/vault/1.0.3/vault_1.0.3_linux_amd64.zip
-unzip vault_1.0.3_linux_amd64.zip
-mv vault /usr/local/bin/vault
-echo "export VAULT_ADDR='http://127.0.0.1:8200'" > /etc/profile.d/vault.sh
-chmod +x /etc/profile.d/vault.sh
-vault server -dev -dev-root-token-id=00000000-0000-0000-0000-000000000000 &
+VAULT_VERSION="1.0.3"
+_VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
+_VAULT_ADDR="http://127.0.0.1:8200"
 
+export VAULT_ADDR=${_VAULT_ADDR}
+export VAULT_TOKEN=${_VAULT_TOKEN}
+
+echo "export VAULT_ADDR=${_VAULT_ADDR}; export VAULT_TOKEN=${_VAULT_TOKEN}" > /etc/profile.d/vault.sh
+chmod +x /etc/profile.d/vault.sh
+
+wget https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+unzip vault_${VAULT_VERSION}_linux_amd64.zip
+chown root:root vault
+mv vault /usr/local/bin
+vault -autocomplete-install
+complete -C /usr/local/bin/vault vault
+useradd --system --home /etc/vault.d --shell /bin/false vault
+mkdir /etc/vault.d
+chown -R vault:vault /etc/vault.d
+
+aws s3 cp s3://repo.bsm.pub/cloud-formation-init/vault.service /etc/systemd/system/vault.service
+
+systemctl enable vault
+systemctl start vault
+
+sleep 10
 vault kv put secret/open-api @/opt/open-api/bsm-secrets.json
 
 systemctl daemon-reload
